c84b0a848998ce46249e28ca883c619e74d90576
Dai Lei <szdailei@gmail.com>
2020-12-03 22:16:40 +0800

    修改演讲者模式测试例

--- packages/test/src/short-test/speaker-mode.spec.js
+++ packages/test/src/short-test/speaker-mode.spec.js
@@ -1,18 +1,17 @@
 const speakModeReq = `
 @pain
-20200601，代磊演示时在便携机和投影仪上显示相同的内容，忘记要说的话。
+20200601，代磊演示时只看到胶片内容，忘记要说的话。
 @expect
-20201111，代磊演示时在便携机上显示演讲备注、投影仪显示胶片内容，方便讲解。
+20201111，代磊演示时将要在便携机上阅读演讲备注，同时投影仪显示胶片内容，方便讲解。
 @status
 20200601，MS Office将ppt文件内容输出到屏幕上。
 @goal
-20201111，在http://127.0.0.1上，浏览器将md文件内容输出到投影仪端口、
-md里面的Note组件内容输出到投影仪端口。`;
+20201111，在http://127.0.0.1上，md文件的内容将要输出到投影仪端口、
+md里面的Note组件内容将要输出到投影仪端口。`;
 describe(speakModeReq, () => {
   const splitScreenReq = `按动P键，浏览器创建新tab显示md文件的Note。
 新tab输出到主显示端口，原tab输出到次端口。
 切换到新tab。
 原tab和新tab同步`;
-  test.skip(splitScreenReq, async () => {
-  });
+  test.skip(splitScreenReq, async () => {});
 });
