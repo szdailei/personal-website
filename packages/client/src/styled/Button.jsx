8f680177b06df4756337830589a46b2b50f541b5
Dai Lei <szdailei@gmail.com>
2020-12-04 13:31:50 +0800

    Add ripple style on Button

--- packages/client/src/styled/Button.jsx
+++ packages/client/src/styled/Button.jsx
@@ -3,6 +3,31 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
 function Button({ onClick, value, children, ...props }) {
+  const RIPPLE_STYLE = {
+    position: 'relative',
+    overflow: 'hidden',
+    transform: 'translate3d(0, 0, 0)',
+
+    '&:after': {
+      content: '""',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      backgroundImage: 'radial-gradient(circle, #fff 5%, transparent 10.01%)',
+      transform: 'scale(10, 10)',
+      opacity: '0',
+      transition: 'transform 0.5s, opacity 1s',
+    },
+
+    '&:active::after': {
+      transform: 'scale(0, 0)',
+      opacity: '0.2',
+      transition: '0s',
+    },
+  };
+
   const objStyles = {
     cursor: 'pointer',
     width: 'fit-content',
@@ -16,6 +41,7 @@ function Button({ onClick, value, children, ...props }) {
       boxShadow:
         '0 0 2px -2px rgba(29, 39, 231, 0.1), 0 0 3px 0 rgba(29, 39, 231, 0.1), 0 0 5px 0 rgba(29, 39, 231, 0.1), 0 2px 2px -4px rgba(29, 39, 231, 0.1), 0 4px 8px 0 rgba(29, 39, 231, 0.1), 0 2px 15px 0 rgba(29, 39, 231, 0.1)',
     },
+    ...RIPPLE_STYLE,
     ...props,
   };
 
